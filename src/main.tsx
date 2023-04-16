61fbc01d5b6b8e660884e572a47378f26f10a2d1
Danilo Morales <danilo.morales@perficient.com>
2023-04-16 15:08:11 -0500

    feature(components): added export components

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import Btn from './components/Btn'
+import Card from './components/Card'
+import Loader from './components/Loader/Loader'
+import InlineLoader from './components/Loader/InlineLoader'
+import ComponentLoader from './components/Loader/ComponentLoader'
+import SystemFeedback from './components/SystemFeedback'
 
-import './index.css'
-import Btn from './components/Btn/Btn'
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-   <Btn className='text-center'>Hola</Btn>
-  </React.StrictMode>,
-)
+export {
+  Btn,
+  Card,
+  Loader,
+  InlineLoader,
+  ComponentLoader,
+  SystemFeedback
+}
