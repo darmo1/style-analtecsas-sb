2bce574ce66eddcdcd81db824b236f5048a72f6a
Danilo Morales <danilo.morales@perficient.com>
2023-04-16 21:04:58 -0500

    feat(styles)

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,8 @@ module.exports = {
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
     "./src/**/**.{js,jsx,ts,tsx}",
-    "./node_modules/style-analtecsas-sb/**/*.{ts,tsx, js, jsx}"
+    "./node_modules/style-analtecsas-sb/src/**/*.{ts,tsx, js, jsx}",
+    path.join(path.dirname(require.resolve(style-analtecsas-sb)), '/**/*.{ts,tsx, js, jsx}')
   ],
   theme: {
     extend: {},
